6b8044aa15192e29bedf3fdcf10187a9c30cbfd7
Vincent Lavallee <vincentlavallee@aol.com>
2021-02-02 13:47:37 -0500

    fix router

--- client/src/components/budget/resume/PieChart.vue
+++ client/src/components/budget/resume/PieChart.vue
@@ -3,12 +3,7 @@
     <div class="title">
       <p class="title is-3">Visualisation</p>
     </div>
-    <div class="relative">
-      <canvas id="myChart"></canvas>
-      <div class="absolute-center text-center">
-        <p class="has-text-link">Taux epargne <br> {{savingsRate.toFixed(2)}}%</p>
-      </div>
-    </div>
+    <canvas id="myChart"></canvas>
   </div>
 </template>
 
@@ -50,6 +45,9 @@ export default {
               text: '99 %',
             },
           },
+          legend: {
+            position: 'left',
+          },
         },
       });
     },
@@ -61,30 +59,6 @@ export default {
 </script>
 
 <style lang="scss">
-.relative {
-  position: relative;
-}
-
-.absolute-center {
-  position:absolute;
-  top: 55%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.text-center{
-  text-align: center;
-}
-
-p {
-  font-size: 1rem;
-  font-weight: bold;
-}
-
-#myChart {
-  width:50%;
-}
-
 #piechart {
   padding: 10px;
   margin: 10px 0px 10px 10px;
